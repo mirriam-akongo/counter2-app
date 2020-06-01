28f94aa606c155e3bac802c730c2deef008c2246
mirriam-akongo <jemmirriako@gmail.com>
2020-06-01 12:53:37 +0300

    commited fully functiontional counter

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import Navbar from "./components/navbar";
+import "./App.css";
+import Counters from "./components/counters";
+import { render } from "@testing-library/react";
+
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 5 },
+      { id: 2, value: 0 },
+      { id: 3, value: 2 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  constructor() {
+    super();
+    console.log("App - Constructor");
+  }
+
+  componentDidMount() {
+    console.log("App - Mounted");
+  }
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  render() {
+    console.log("App - Rendered");
+    return (
+      <div>
+        <Navbar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
